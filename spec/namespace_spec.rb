2389604926f8e388961d96234b45c86a58bb9745
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-07-03 16:50:20 +0100

    Add additional namespace tests

--- spec/namespace_spec.rb
+++ spec/namespace_spec.rb
@@ -1,18 +1,38 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe "namespace" do
 
-  def can_i_get(type)
-    `kubectl auth can-i get #{type} --namespace kube-system --as test --as-group github:webops --as-group system:authenticated`.chomp
+  def can_i_get(type, team, namespace = "kube-system")
+    `kubectl auth can-i get #{type} --namespace #{namespace} --as test --as-group github:#{team} --as-group system:authenticated`.chomp
   end
 
   it "allows webops to access namespace" do
-    result = can_i_get "namespace"
+    result = can_i_get "namespace", "webops"
     expect(result).to eq("yes")
+  end
+  
+  it "does not allow non-webops to access namespace" do
+    result = can_i_get "namespace", "not-webops"
+    expect(result).to eq("no")
   end
 
   it "allows webops to access pods" do
-    result = can_i_get "pod"
+    result = can_i_get "pod", "webops"
+    expect(result).to eq("yes")
+  end
+  
+  it "does not allow non-webops to access pods" do
+    result = can_i_get "pod", "not-webops"
+    expect(result).to eq("no")
+  end
+  
+  it "allows non-webops to access pods" do
+    result = can_i_get "pod", "offender-management", "offender-management-staging"
+    expect(result).to eq("yes")
+  end
+  
+  it "allows non-webops to access namespace" do
+    result = can_i_get "namespace", "offender-management", "offender-management-staging"
     expect(result).to eq("yes")
   end
 end
