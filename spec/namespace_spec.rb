1432b3511f0e85ed8a67d9aafc742c31a147f1ea
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-07-03 16:25:05 +0100

    Refactor namespace test

--- spec/namespace_spec.rb
+++ spec/namespace_spec.rb
@@ -1,10 +1,18 @@
 require "spec_helper"
 
 describe "namespace" do
+
+  def can_i_get(type)
+    `kubectl auth can-i get #{type} --namespace kube-system --as test --as-group github:webops --as-group system:authenticated`.chomp
+  end
+
   it "allows webops to access namespace" do
-    result = `kubectl auth can-i get namespace --namespace kube-system --as test --as-group github:webops --as-group system:authenticated`.chomp
+    result = can_i_get "namespace"
     expect(result).to eq("yes")
   end
-  it "allows webops to access pods"
 
+  it "allows webops to access pods" do
+    result = can_i_get "pod"
+    expect(result).to eq("yes")
+  end
 end
